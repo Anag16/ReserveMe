7c8c6a160501b5e279d35417ae513dd30e7e8090
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-10-24 17:03:13 -0400

    Add login, registration and header JavaScript files

---
+++ reserve_app/src/components/admin/_header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header_Admin(props) {
+
+  return (
+    <div className="Header_Admin">
+      <h1>Header_Admin</h1>
+    </div>
+  )
+}
---
+++ reserve_app/src/components/client/registration.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Registration(props) {
+
+  return (
+    <div className="Registration">
+      <h1>Registration</h1>
+    </div>
+  )
+}
