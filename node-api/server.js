c502e1c38deec7416401d86be66da21e25ebd964
denizedaza <dazadenize@gmail.com>
2021-10-24 17:30:16 -0400

    add cors to backend

--- node-api/server.js
+++ node-api/server.js
@@ -2,7 +2,12 @@ const PORT = 3003;
 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+
+//middleware 
+app.use(morgan('dev'));
+// app.use(cors()) - to use for later
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
