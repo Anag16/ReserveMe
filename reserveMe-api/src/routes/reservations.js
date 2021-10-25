02d435d95e6c642f15ffaef97bc1f325cad0459b
M <79332342+MM0nty@users.noreply.github.com>
2021-10-25 18:27:15 +0000

    Attempt to update and delete reservations from database

--- reserveMe-api/src/routes/reservations.js
+++ reserveMe-api/src/routes/reservations.js
@@ -2,15 +2,41 @@ const router = require("express").Router();
 
 module.exports = db => {
   router.get("/reservations", (request, response) => {
-    db.query(`SELECT * FROM reservations`).then(({ rows: interviewers }) => {
+    db.query(
+      `SELECT *
+      FROM reservations`
+    ).then(({ rows: reservations }) => {
       response.json(
-        interviewers.reduce(
+        reservations.reduce(
           (previous, current) => ({ ...previous, [current.id]: current }),
           {}
         )
       );
     });
+  })
+
+  router.put("/reservations/:id", (request, response) => {
+    const { reservation_date, start_time, end_time } = request.body.reservation;
+    db.query(
+      `
+      INSERT INTO reservations (reservation_date, start_time, end_time, user_id, store_id)
+      VALUES ($1::date, $2::timez, $3::timez, $4::integer, $5::integer)
+      ON CONFLICT (reservation_id)
+      UPDATE SET reservation_date = $1::date, start_time = $2::text, end_time = $3::text
+      `, [reservation_date, start_time, end_time]
+    ).then(() => {
+      response.status(204).json({})
+    })
+  })
+
+  router.delete("/reservations/:id", (request, response) => {
+    db.query(
+      `DELETE FROM reservations
+      WHERE reservation_id = $1::integer`,
+      [request.params.id]
+    ).then(() => {
+      response.status(204).json({})
+    })
   });
-  
   return router;
-};
+}
