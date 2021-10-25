5d06706efd5138e9ca443f42b3184cae86099c53
denizedaza <dazadenize@gmail.com>
2021-10-25 16:04:43 -0400

    change files and imports for sass

--- reserve_app/src/index.js
+++ reserve_app/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "index.scss";
+import "./index.scss";
 
-import App from "/App";
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
