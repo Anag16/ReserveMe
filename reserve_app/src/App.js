07e6b2e9d61cfa2267f7a607344d524a79afe829
Ana Ruiz <ruizana393@gmail.com>
2021-10-24 21:48:10 -0400

    Login Route

--- reserve_app/src/App.js
+++ reserve_app/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../src/components/admin/dashboard';
 import Store from '../src/components/client/store';
+import Login from '../src/components/Login/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
