8dce5eee6a0a4789b8ae062aae7d9596b3dd4008
M <79332342+MM0nty@users.noreply.github.com>
2021-10-26 17:27:53 +0000

    Fix layout of admin dashboard

--- reserve_app/src/components/admin/dashboard.js
+++ reserve_app/src/components/admin/dashboard.js
@@ -5,16 +5,20 @@ export default function Dashboard(props) {
   return (
     <main className="layout">
       <h1>Dashboard</h1>
-      <section className="sidebar">
+      <section className="nav">
         <nav className="menu">Search | List | Reservations</nav>
       </section>
-      <section className="dashboard">
+      <section className="sidebar">
         <tr>
           <td>Dashboard</td>
           <td>Store page</td>
           <td>Clicker</td>
           <td>Admin menu option</td>
         </tr>
+      </section>
+      <section className="dashboard">
+        <h2>Pertinent information group</h2>
+        <h3>Pertinent information</h3>
       </section>
     </main>
   )
