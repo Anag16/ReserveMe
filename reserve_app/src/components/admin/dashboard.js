a63e10a29e210509c296235b9e383d8e4a9ee97e
M <79332342+MM0nty@users.noreply.github.com>
2021-10-26 17:08:09 +0000

    Remove admin_header and start general outline for admin pages

--- reserve_app/src/components/admin/dashboard.js
+++ reserve_app/src/components/admin/dashboard.js
@@ -3,8 +3,19 @@ import React from "react";
 export default function Dashboard(props) {
 
   return (
-    <div className="Dashboard">
+    <main className="layout">
       <h1>Dashboard</h1>
-    </div>
+      <section className="sidebar">
+        <nav className="menu">Search | List | Reservations</nav>
+      </section>
+      <section className="dashboard">
+        <tr>
+          <td>Dashboard</td>
+          <td>Store page</td>
+          <td>Clicker</td>
+          <td>Admin menu option</td>
+        </tr>
+      </section>
+    </main>
   )
 }
