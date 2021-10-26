5d438c966d6053596a3eaf2e895a893f359cb458
denizedaza <dazadenize@gmail.com>
2021-10-26 17:14:25 -0400

    restore store search changes

---
+++ reserve_app/src/components/client/search.js
@@ -0,0 +1,28 @@
+import { TextField } from "@mui/material";
+import { Button } from "@mui/material";
+
+export default function Search(props) {
+  const { searchQuery, setSearchQuery } = props;
+
+  return (
+    <form action="/" method="get">
+      <label htmlFor="store-Search">
+        <span className="hidden">Search stores</span>
+      </label>
+      {/* <input  */}
+        <TextField 
+        id="outlined-basic" label="Search..." variant="outlined"
+        value={ searchQuery }
+        onInput={event => setSearchQuery(event.target.value)}
+        type="text"
+        id="store-search"
+        placeholder="Search for stores"
+        name="search"
+        />
+        {/* <button type="submit">Search</button> */}
+        <Button variant="contained" size="medium">
+          Search
+        </Button>
+    </form>
+  )
+};
