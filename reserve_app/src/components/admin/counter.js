5fc9cb1f6384ec4b247c1110b1872371468cfbed
M <79332342+MM0nty@users.noreply.github.com>
2021-10-26 17:15:01 +0000

    Add general outline for admin_counter page

--- reserve_app/src/components/admin/counter.js
+++ reserve_app/src/components/admin/counter.js
@@ -3,8 +3,21 @@ import React from "react";
 export default function Counter(props) {
 
   return (
-    <div className="Counter">
+    <main className="Counter">
       <h1>Counter</h1>
-    </div>
+      <img
+        className="counter_add"
+        src="images/add.png"
+        alt="Add"
+        onClick={props.onAdd}
+      />
+      <h1>{props.counnt}</h1>
+      <img
+        className="counter_reduce"
+        src="images/reduce.png"
+        alt="Reduce"
+        onClick={props.onReduce}
+      />
+    </main>
   )
 }
