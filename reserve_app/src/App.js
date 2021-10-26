51c3ca02713e198649f58c08aa1572aa6836c3cc
mint <ruizana393@gmail.com>
2021-10-26 01:16:34 -0400

    Removed Jest

--- reserve_app/src/App.js
+++ reserve_app/src/App.js
@@ -1,33 +1,27 @@
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../src/components/admin/dashboard';
 import Store from '../src/components/client/store';
 import Login from '../src/components/Login/Login';
 import useToken from '../src/components/App/useToken';
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
 
-function App() {
-  const { token, setToken } = useToken();
-
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
-
-  return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
         <Switch>
-          <Route path="/admin/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/store">
-            <Store />
-          </Route>
+          {/* <Route path="/" exact component={Home} /> */}
+          <Route path="/login" component={Login} />
         </Switch>
-      </BrowserRouter>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
