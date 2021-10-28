57740f610ff4d5b3f679ac9b23f92b50dff99fd7
M <79332342+MM0nty@users.noreply.github.com>
2021-10-28 22:56:51 +0000

    Detail admin dashboard (front-end)

--- reserve_app/src/components/admin/dashboard.js
+++ reserve_app/src/components/admin/dashboard.js
@@ -1,24 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dashboard(props) {
+  const [storeName, setStoreName] = useState(props.storeName || "");
+  const [storeDescription, setStoreDescription] = useState(props.storeDescription || "");
+  const [safetyMeasures, setSafetyMeasures] = useState(props.safetyMeasures || "");
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  const save = () => {
+    save();
+    props.onSave();
+  }
 
   return (
     <main className="layout">
       <h1>Dashboard</h1>
-      <section className="nav">
-        <nav className="menu">Search | List | Reservations</nav>
-      </section>
       <section className="sidebar">
         <tr>
           <td>Dashboard</td>
+          <br />
           <td>Store page</td>
+          <br />
           <td>Clicker</td>
+          <br />
           <td>Admin menu option</td>
         </tr>
       </section>
       <section className="dashboard">
         <h2>Pertinent information group</h2>
         <h3>Pertinent information</h3>
+      </section>
+      <form onSubmit={event => event.preventDefault()}>
+        <section>
+          <input
+            className="store-name"
+            name="name"
+            type="text"
+            placeholder="Enter store Name"
+            value={storeName}
+            onChange={(event) => setStoreName(event.target.value)}
+            data-testid="store-name-input"
+          />
+        </section>
+        <section className="description">
+          <input
+            className="store-description"
+            name="name"
+            type="text"
+            placeholder="Enter store description"
+            value={storeDescription}
+            onChange={(event) => setStoreDescription(event.target.value)}
+            data-testid="store-description-input"
+          />
+        </section>
+        <section className="store-image">
+          Store image
+        </section>
+        <section className="store-location">
+          Store location
+        </section>
+        <section className="store-capacity">
+          Store capacity
+        </section>
+        <section className="safety-measures">
+          <input
+            className="safety-measures"
+            name="name"
+            type="text"
+            placeholder="Enter store safety measures"
+            value={safetyMeasures}
+            onChange={(event) => setSafetyMeasures(event.target.value)}
+            data-testid="safety-measures-input"
+          />
+        </section>
+        <section className="opening-hours">
+          Opening hours
+        </section>
+        <section className="closing-hours">
+          Closing hours
+        </section>
+      </form>
+      <section className="store-buttons">
+        <button onClick={cancel}>Cancel changes</button>
+        <button onClick={save}>Save changes</button>
       </section>
     </main>
   )
