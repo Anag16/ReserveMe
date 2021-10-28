b0d5f6561683e44ebe49e4e1f7f69b2c062fd3cb
mint <ruizana393@gmail.com>
2021-10-28 17:05:48 -0400

    Store Reservations by Date

--- reserve_app/src/components/client/day-reservations-item.js
+++ reserve_app/src/components/client/day-reservations-item.js
@@ -1,13 +1,33 @@
-import { Link } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 export default function DayReservationItem(props) {
   const { store_id, user_id, dateString } = props;
 
+  const [isLoading, setLoading] = useState(true);
+  const [availableDays, setAvailableDays] = useState();
+
+  let day = new Date(dateString);
+  useEffect(() => {
+    axios.get(`/api/reservations/${store_id}/${day.toISOString()}`)
+    .then(res => {
+     if (res.status === 200) {
+       setAvailableDays(res.data);
+       setLoading(false);
+     } else {
+       const error = new Error(res.error);
+       throw error;
+     }
+    })
+     .catch(err => {
+       console.error(err);
+       alert('Error. Please try again');
+     });
+  });
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
   return (
     <div>
       <p>The date is: {dateString}</p>
