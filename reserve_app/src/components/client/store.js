a1542d3929aa1612a588c0b7da1fcd171d84f1de
Ana Ruiz <ruizana393@gmail.com>
2021-10-28 00:38:30 -0400

    Axios store details request

--- reserve_app/src/components/client/store.js
+++ reserve_app/src/components/client/store.js
@@ -1,15 +1,40 @@
-import React from "react";
 import { useLocation } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 export default function Store(props) {
 
   const location = useLocation()
-  const { store_id } = location.state
+  const { store_id } = location.state;
+  const [isLoading, setLoading] = useState(true);
+  const [store, setStore] = useState();
 
+  useEffect(() => {
+    axios.get(`/api/stores/${store_id}`)
+    .then(res => {
+     if (res.status === 200) {
+       setStore(res.data);
+       setLoading(false);
+     } else {
+       const error = new Error(res.error);
+       throw error;
+     }
+    })
+     .catch(err => {
+       console.error(err);
+       alert('Error. Please try again');
+     });
+  });
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
+
+  let storeObj = store[0];
   return (
     <div className="Store">
       <h1>Store</h1>
-      <p>This store's id is: {store_id}</p>
+      <p>This store's name is: {storeObj.name}</p>
     </div>
   )
 }
