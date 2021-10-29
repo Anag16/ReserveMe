4674fc3cf4cf4dde2e6964a4c2ed5d3a5b1b1112
denizedaza <dazadenize@gmail.com>
2021-10-28 22:39:21 -0400

    add emojis and conditionals

--- reserve_app/src/components/App/Nav.js
+++ reserve_app/src/components/App/Nav.js
@@ -5,14 +5,14 @@ function Nav () {
   const [cookie, updateCookie] = useCookie('token');
 //   let [authToken, setToken] = useState(0); 
 
-    let homePage = cookie ?  <NavLink to="/">Home 🏠 </NavLink>: '';
-    let storeList = cookie ? <NavLink to="/stores">List of stores 🏠</NavLink> : '';
+    let homePage = cookie ?  <NavLink to="/">Home 🏠 | </NavLink>: '';
+    let storeList = cookie ? <NavLink to="/stores">List of stores 🏘 | </NavLink> : '';
     // let store = cookie ? <NavLink to="/store">Store 🏗 </NavLink> : '';
-    let reservation = cookie ?  <NavLink to="/reservation">Reservation 📅 </NavLink> : '';
-    let dashboard = cookie ?  <NavLink to="/admin/dashboard">Dashboard 🐗 </NavLink> : '';
-    let login = !cookie ?   <NavLink to="/login">Login 🚪 </NavLink>: '';
-    let logout = cookie ?  <NavLink to="/logout"  onClick={() => {updateCookie(null);}}>Logout 📅 </NavLink>: '';
-    let register = !cookie ?  <NavLink to="/register">Register ✏ </NavLink>: '';
+    let reservation = cookie ?  <NavLink to="/reservation">Reservations 📅 | </NavLink> : '';
+    let dashboard = cookie ?  <NavLink to="/admin/dashboard">Dashboard 🐗 | </NavLink> : '';
+    let login = !cookie ?   <NavLink to="/login">Login 🚪| </NavLink>: '';
+    let logout = cookie ?  <NavLink to="/logout"  onClick={() => {updateCookie(null);}}>Logout 📤 | </NavLink>: '';
+    let register = !cookie ?  <NavLink to="/register">Register 📜 </NavLink>: '';
 
   return (
     <div>
