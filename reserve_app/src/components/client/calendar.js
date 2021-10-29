f43ce3c312f77fc4fa57afe7b098104ccaf1ed44
Ana Ruiz <ruizana393@gmail.com>
2021-10-29 00:35:10 -0400

    Calendar Slots

--- reserve_app/src/components/client/calendar.js
+++ reserve_app/src/components/client/calendar.js
@@ -13,39 +13,42 @@ export default function Calendar(props) {
     axios.get(`/api/reservations`)
     .then(res => {
      if (res.status === 200) {
-       setLoading(false);
-       console.log(res.data);
       let resultArray = [];
       let calendarObj = {};
-
       // for loop through res.data and extract values to remap
       for (const obj of res.data) {
         // console.log(obj);
-        const dateVal = obj.reservation_date.slice(0,10)
-        // console.log(dateVal);
+        let startTime = new Date(obj.reservation_date);
+        startTime.setHours(obj.start_hour);
+        startTime.setMinutes(obj.start_minutes);
 
+        let endTime = new Date(obj.reservation_date);
+        endTime.setHours(obj.end_hour);
+        endTime.setMinutes(obj.end_minutes);
         calendarObj = {
-          title: "Booked slot",
-          start: dateVal,
-          end: dateVal,
-          // startTime: `${obj.start_hour}:${obj.start_minutes}`,
-          // endTime: `${obj.end_hour}:${obj.end_minutes}`
-          startTime: {
-            hour: obj.start_hour,
-            minute: obj.start_minutes
-          },
-          endTime: {
-            hour: obj.end_hour,
-            minute: obj.end_minutes
-          }
+          title: "Booked slots",
+          start: startTime,
+          end: endTime,
+          // // startTime: `${obj.start_hour}:${obj.start_minutes}`,
+          // // endTime: `${obj.end_hour}:${obj.end_minutes}`
+          // startTime: {
+          //   hour: obj.start_hour,
+          //   minute: obj.start_minutes
+          // },
+          // endTime: {
+          //   hour: obj.end_hour,
+          //   minute: obj.end_minutes
+          // }
         };
         resultArray.push(calendarObj);
       }
-      
-      console.log(typeof calendarObj.start);
-      console.log("Results Array:",resultArray);
+      // console.log(calendarObj);
+      // console.log("Results Array:",resultArray);
 
        setReservations(resultArray);
+       setLoading(false);
+       console.log(resultArray);
+       console.log(reservations);
      } else {
        const error = new Error(res.error);
        throw error;
@@ -55,17 +58,21 @@ export default function Calendar(props) {
        console.error(err);
        alert('Error. Please try again');
      });
-  }, []);
+  }, [reservations]);
 
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
   // console.log(reservations.reservation_date.slice(0,10));
-
+  console.log(reservations);
   return (
     <>
       <FullCalendar 
          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
          initialView="dayGridMonth"
          headerToolbar={{center: 'dayGridMonth,timeGridWeek,timeGridDay'}}
-         events={reservations}
+         events = {reservations}
          nowIndicator
       />
     </>
