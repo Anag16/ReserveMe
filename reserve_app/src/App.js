c22e03f0a2c1cfc50940c853eb0f080cc87b82fe
M <79332342+MM0nty@users.noreply.github.com>
2021-11-01 21:10:56 +0000

    Fix seeds error, add basic home page, and detail reservations more

--- reserve_app/src/App.js
+++ reserve_app/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
 import {Route, Switch } from 'react-router-dom';
-import AuthComponent from '../src/components/AuthComponent'
-import Dashboard from '../src/components/admin/dashboard';
-import Store from '../src/components/client/store';
-import StoreList from './components/client/store-list';
+import AuthComponent from './components/AuthComponent'
+import Home from './components/Home'
+import Nav from './components/Nav';
+import Dashboard from './components/admin/dashboard';
 import Reservations from './components/admin/reservations';
 import Counter from './components/admin/counter'
-import Nav from './components/Nav';
-import Login from '../src/components/Login/Login';
-import Logout from '../src/components/Login/Logout';
-import Register from '../src/components/Register/Register';
+import Store from './components/client/store';
+import StoreList from './components/client/store-list';
+import Login from './components/Login/Login';
+import Logout from './components/Login/Logout';
+import Register from './components/Register/Register';
+
 
 class App extends Component {  
   render() {
@@ -18,15 +20,15 @@ class App extends Component {
       <div>
         <Nav> </Nav>
         <Switch>
-          {/* <Route path="/" exact component={Home} /> */}
+          <Route path="/" exact component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/logout" component={Logout} />
           <Route path="/register" component={Register} />
           <Route path="/admin/dashboard" component={AuthComponent(Dashboard)} />
+          <Route path="/admin/reservations" component={AuthComponent(Reservations)} />
           <Route path="/admin/counter" component={AuthComponent(Counter)} />
           <Route path="/stores" component={AuthComponent(StoreList)} />
           <Route path="/store" component={AuthComponent(Store)} />
-          <Route path="/admin/reservations" component={AuthComponent(Reservations)} />
         </Switch>
       </div>
     );
