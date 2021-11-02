2a6e89138d9936d630546b030c5bb950e45af410
M <79332342+MM0nty@users.noreply.github.com>
2021-11-02 00:27:17 +0000

    Attempt to get stores table's information from database

--- reserveMe-api/routes/admin/dashboard.js
+++ reserveMe-api/routes/admin/dashboard.js
@@ -7,19 +7,9 @@ module.exports = db => {
     db.query(
       `
       SELECT
-      store_id,
-      name,
-      description,
-      image,
-      location,
-      capacity,
-      customer_count,
-      reservation_capacity,
-      safety_measures,
-      opening_hour,
-      closing_hour,
-      is_admin
-    FROM stores WHERE store_id = ${store_id}
+      stores.*,
+    FROM stores
+    JOIN admins ON stores.admin_id = admins.admin_id WHERE store_id = ${store_id} && admin_id = ${admin_id}
     `
     ).then(({ rows: store }) => {
       response.json(store);
@@ -28,15 +18,15 @@ module.exports = db => {
 
   //reservation_capacity = number of reservations allowed or number of reservations used??
   router.put("/admin/:store_id", (request, response) => {
-    const { name, description, image, location, capacity, customer_count, reservation_capacity, safety_measures, opening_hour, closing_hour, is_admin } = request.body.store;
+    const { name, description, image, location, capacity, customer_count, reservation_capacity, safety_measures, opening_hour, closing_hour, admin_id} = request.body.store;
     db.query(
       `INSERT INTO stores
       (name, description, image, location, capacity, customer_count, reservation_capacity, safety_measures, opening_hour, closing_hour)
       VALUES
       ($1::text, $2::text, $3::text, $4::text, $5::integer, $6::integer, $7::integer, $8::text, $9::integer, $10::integer, $11::boolean)
       ON CONFLICT () DO
-      UPDATE SET name = $1::text, description = $2::text, image = $3::text, location = $4::text, capacity = $5::integer, customer_count = $6::integer, reservation_capacity = $7::integer, safety_measures = $8::text, opening_hour = $9::integer, closing_hour = $10::integer, is_admin = $11::boolean)`,
-      [name, description, image, location, capacity, customer_count, reservation_capacity, safety_measures, opening_hour, closing_hour, is_admin]
+      UPDATE SET name = $1::text, description = $2::text, image = $3::text, location = $4::text, capacity = $5::integer, customer_count = $6::integer, reservation_capacity = $7::integer, safety_measures = $8::text, opening_hour = $9::integer, closing_hour = $10::integer, admin_id = $11::integer)`,
+      [name, description, image, location, capacity, customer_count, reservation_capacity, safety_measures, opening_hour, closing_hour, admin_id]
     )
       .then(() => {
         response.json(store)
