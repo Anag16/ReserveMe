e1008f0e4db9f68d5d21dc3e764bda3f6d3f87a6
Ana Ruiz <ruizana393@gmail.com>
2021-11-02 23:33:47 -0400

    HomePage Initial

--- reserve_app/src/components/Home.js
+++ reserve_app/src/components/Home.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
+import './Home.css';
 
 export default function Home(props) {
 
-  return (
-    <div className="Home">
-      <h1>Home</h1>
+  return(
+    <div className="home-wrapper">
+      <div id="heroText">
+        <h1 id="mainName">ReserveMe</h1>
+        <p id="sloganMain">You don't need to wait, skip the line.</p>
+        <div className="featuredbttns">
+            <a className="btn" href="/stores">Your Favourite Stores </a>
+        </div>
+      </div>
+      <div id="emptyBlock">
+
+      </div>
+        
     </div>
   )
 }
+
+Home.propTypes = {
+ 
+};
